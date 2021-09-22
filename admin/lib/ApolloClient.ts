b73f217007d2c79211fcc5ed8d584a8f7410232b
irere123 <irere2020@outlook.com>
2021-09-23 01:19:50 +0200

    vercel deploy

--- admin/lib/ApolloClient.ts
+++ admin/lib/ApolloClient.ts
@@ -5,9 +5,7 @@ const __prod__ = process.env.NODE_ENV === "production";
 export const client = new ApolloClient({
   ssrMode: true,
   link: createHttpLink({
-    uri: !__prod__
-      ? "http://localhost:5050/graphql"
-      : process.env.NEXT_PUBLIC_API_URL,
+    uri: !__prod__ ? "http://localhost:5050/graphql" : process.env.API_URL,
     credentials: "include",
   }),
   cache: new InMemoryCache(),
