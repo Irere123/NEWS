782f931b219825c702b29d0393bd6449546d57fa
irere123 <irere2020@outlook.com>
2021-09-23 01:31:14 +0200

    vercel deploy

--- admin/lib/ApolloClient.ts
+++ admin/lib/ApolloClient.ts
@@ -1,13 +1,9 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 
-const __prod__ = process.env.NEXT_PUBLIC_NODE_ENV === "production";
-
 export const client = new ApolloClient({
   ssrMode: true,
   link: createHttpLink({
-    uri: !__prod__
-      ? "http://localhost:5050/graphql"
-      : "https://api-news-graphql.herokuapp.com/graphql",
+    uri: "http://localhost:5050/graphql",
     credentials: "include",
   }),
   cache: new InMemoryCache(),
