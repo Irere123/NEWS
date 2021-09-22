c3612086c8295f4ba61f7f91bfd34e83aef580ed
irere123 <irere2020@outlook.com>
2021-09-22 22:38:36 +0200

    fix: made the login and register page

--- admin/pages/login.tsx
+++ admin/pages/login.tsx
@@ -1,18 +1,45 @@
 import Head from "next/head";
-import { Email } from "@mui/icons-material";
+import Link from "next/link";
+import { EmailOutlined, LockOutlined } from "@mui/icons-material";
 
 import styles from "../styles/Registration.module.css";
 
 function LoginForm() {
   return (
-    <>
-      <div className={styles.InputField}>
-        <span>
-          <Email />
-        </span>
-        <input type="email" name="email" />
+    <div className={styles.LoginForm}>
+      <div>
+        <label className={styles.InputLabel}>Email</label>
+        <div className={styles.InputField}>
+          <span>
+            <EmailOutlined />
+          </span>
+          <input type="email" name="email" />
+        </div>
       </div>
-    </>
+      <div>
+        <label className={styles.InputLabel}>Password</label>
+        <div className={styles.InputField}>
+          <span>
+            <LockOutlined />
+          </span>
+          <input type="password" name="password" />
+        </div>
+      </div>
+      <div className={styles.LoginFormLinks}>
+        <Link href="/change-password">
+          <a>Forgot Password</a>
+        </Link>
+        <h3>Or</h3>
+        <Link href="/register">
+          <a>Create new account</a>
+        </Link>
+      </div>
+      <div className={styles.ButtonContainer}>
+        <button type="submit" className={styles.Button}>
+          Login
+        </button>
+      </div>
+    </div>
   );
 }
 
@@ -24,9 +51,9 @@ function Login() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Login - NEWS</title>
       </Head>
-      <main className={styles.LoginContainer}>
+      <main className={styles.Container}>
         <div className={styles.LoginCard}>
-          <h2 className={styles.LoginCardTitle}>LOGIN</h2>
+          <h2 className={styles.CardTitle}>LOGIN</h2>
           <LoginForm />
         </div>
       </main>
--- admin/pages/register.tsx
+++ admin/pages/register.tsx
@@ -1,18 +1,71 @@
 import Head from "next/head";
+import Link from "next/link";
+import {
+  EmailOutlined,
+  LockOutlined,
+  PersonOutlined,
+} from "@mui/icons-material";
+
+import styles from "../styles/Registration.module.css";
+
+function RegisterForm() {
+  return (
+    <div className={styles.RegisterForm}>
+      <div>
+        <label className={styles.InputLabel}>Username</label>
+        <div className={styles.InputField}>
+          <span>
+            <PersonOutlined />
+          </span>
+          <input type="text" name="username" />
+        </div>
+      </div>
+      <div>
+        <label className={styles.InputLabel}>Email</label>
+        <div className={styles.InputField}>
+          <span>
+            <EmailOutlined />
+          </span>
+          <input type="email" name="email" />
+        </div>
+      </div>
+      <div>
+        <label className={styles.InputLabel}>Password</label>
+        <div className={styles.InputField}>
+          <span>
+            <LockOutlined />
+          </span>
+          <input type="password" name="password" />
+        </div>
+      </div>
+      <div className={styles.LoginFormLinks}>
+        <Link href="/login">
+          <a>Already have account ?</a>
+        </Link>
+      </div>
+      <div className={styles.ButtonContainer}>
+        <button type="submit" className={styles.Button}>
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
+}
 
 function Register() {
   return (
-    <div className="flex items-center justify-center grid-cols-2">
+    <div>
       <Head>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Create Account - NEWS</title>
+        <title>Register - NEWS</title>
       </Head>
-      <div>
-        <div className=" bg-white shadow">
-          <h1>Register Account</h1>
+      <main className={styles.Container}>
+        <div className={styles.RegisterCard}>
+          <h2 className={styles.CardTitle}>CREATE ACCOUNT </h2>
+          <RegisterForm />
         </div>
-      </div>
+      </main>
     </div>
   );
 }
