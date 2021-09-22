1e011ae9f0438716bcc94ac8c519fb507f39f599
irere123 <irere2020@outlook.com>
2021-09-23 01:34:02 +0200

    changed apu url

--- admin/lib/ApolloClient.ts
+++ admin/lib/ApolloClient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
   ssrMode: true,
   link: createHttpLink({
-    uri: "http://localhost:5050/graphql",
+    uri: "https://api-news-graphql.herokuapp.com/graphql",
     credentials: "include",
   }),
   cache: new InMemoryCache(),
